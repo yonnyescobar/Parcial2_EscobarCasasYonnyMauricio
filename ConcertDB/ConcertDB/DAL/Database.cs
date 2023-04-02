40ad4ada7f7f304f83f2bf329c0535d57a8dfbb0
Yonny Escobar <yonnyescobar@Kakaroto.local>
2023-04-02 10:20:56 -0500

    Created Database Context and connection strings

---
+++ ConcertDB/ConcertDB/DAL/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ConcertDB.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConcertDB.DAL
+{
+    public class Database : DbContext
+    {
+        #region constructor
+        public Database(DbContextOptions<Database> options) : base(options)
+        {
+
+        }
+        #endregion
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
