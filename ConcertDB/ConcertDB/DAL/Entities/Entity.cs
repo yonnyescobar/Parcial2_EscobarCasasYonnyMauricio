105d4a0a0b28638531ca07a13a6facdc1b97ce2c
Yonny Escobar <yonnyescobar@Kakaroto.local>
2023-04-02 09:52:06 -0500

    Created the Entity and Ticket Classes

---
+++ ConcertDB/ConcertDB/DAL/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
+{
+    public class Entity
+    {
+        #region porperties
+        [Key]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Número de boleta")]
+        public int Id { get; set; }
+
+        [Display(Name = "Fecha de uso")]
+        public DateTime? UseDate { get; set; }
+        #endregion
+    }
+}
---
+++ ConcertDB/ConcertDB/DAL/Entities/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
+{
+    public class Ticket : Entity
+    {
+        #region properties
+        [Display(Name = "Estado")]
+        public bool IsUsed { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        [Display(Name = "Portería de entrada")]
+        public string? EntranceGate { get; set; }
+        #endregion
+    }
+}
+
